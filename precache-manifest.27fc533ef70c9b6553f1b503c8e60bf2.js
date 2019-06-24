eca5840b1a7a27f4867c417103cccd325e6ec909
Nick <nicolas.risi@saegus.com>
2019-06-24 14:32:43 +0200

    Updates

---
+++ precache-manifest.27fc533ef70c9b6553f1b503c8e60bf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb94c445b9302a26ddba546ab3999211",
+    "url": "/scrapping-lazyloading/index.html"
+  },
+  {
+    "revision": "4e68cc4f4936b368db60",
+    "url": "/scrapping-lazyloading/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "0a11ade0d303d7226539",
+    "url": "/scrapping-lazyloading/static/js/2.62a86639.chunk.js"
+  },
+  {
+    "revision": "4e68cc4f4936b368db60",
+    "url": "/scrapping-lazyloading/static/js/main.fb439427.chunk.js"
+  },
+  {
+    "revision": "3a51803a7092da29bc06",
+    "url": "/scrapping-lazyloading/static/js/runtime~main.4cf018eb.js"
+  }
+]);
